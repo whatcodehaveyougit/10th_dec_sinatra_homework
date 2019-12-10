24c8287fe5484238d50746ac6cb11668b643603f
whatcodehaveyougit <siggywatt@yahoo.com>
2019-12-10 23:27:06 +0000

    finish

--- controller.rb
+++ controller.rb
@@ -4,6 +4,7 @@ require 'sinatra/contrib/all'
 # have to keep re-running it every time.
 require 'pry'
 require_relative('./models/films')
+require_relative('./models/customers')
 also_reload('./models/*')
 # If something in models changes then relaod it
 
@@ -16,6 +17,13 @@ end
 get '/film_info/:id' do
   @film_info = Film.find_film_by_id(params[:id])
   erb(:film_info)
+end
+
+
+# MUST REQUIRE RELATIVE TO BRING IN A NEW CLASS
+get '/customers' do
+  @customers = Customer.read_all()
+  erb(:customers)
 end
 
 # get '/find_info_batman' do
